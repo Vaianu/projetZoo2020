25b852e0f31859ae67eadfc9a660d751e0ba947b
Utilisateur <Utilisateur@Iaorana>
2020-01-27 11:36:05 +0100

    ajout package controleur + class Manager

---
+++ src/main/java/org/formation/zoo/controleur/Manager.java
@@ -0,0 +1,14 @@
+package org.formation.zoo.controleur;
+
+/**
+ * SINGLETON et une FACADE
+ * @author v.samgmouit
+ *
+ */
+public final class Manager {
+
+	public Manager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
