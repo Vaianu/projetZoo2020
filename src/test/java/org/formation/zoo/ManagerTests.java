44fa8ac0435e27812a5801ed808d60615a2018be
Vaianu <Utilisateur@Iaorana>
2020-03-12 19:16:06 +0100

    Test unitaire de la classe Manager

--- src/test/java/org/formation/zoo/ManagerTests.java
+++ src/test/java/org/formation/zoo/ManagerTests.java
@@ -13,11 +13,35 @@ class ManagerTests {
 	@Test
 	void testGetAnimaux() {
 		List<CagePOJO> res = Manager.getInstance().getAnimaux();
-		assertEquals("Baloo 20 ans 75.0 kg", res.get(0).getPancarte());
-		assertEquals("cage vide", res.get(1).getPancarte());
-		assertEquals("images/singe.gif", res.get(0).getImage());
-		assertEquals("images/cage.gif", res.get(1).getImage());
+		double poidsGaga = res.get(0).getPoids();
+		assertEquals("GAGAG 5 ans " + poidsGaga + " kg", res.get(0).getPancarte());
+		assertEquals("cage vide", res.get(4).getPancarte());
+		assertEquals(700, res.get(1).getX());
+		assertEquals(230, res.get(1).getY());
+		assertEquals("Gazelle", res.get(1).getCodeAnimal());
+		assertEquals(5, res.get(1).getCle());
+		assertEquals("./images/gazelle.gif", res.get(0).getImage());
+		assertEquals("./images/cage.jpg", res.get(4).getImage());
 		// FINIR LES TESTS§§§§§§§§
+	}
+	
+	@Test
+	void testGetInstance() {
+		assertNotNull(Manager.getInstance());
+	}
+	
+	@Test
+	void testAfficher() {
+		String expected = "Cage [x=300, y=300]----VIDE";
+		assertEquals(expected, Manager.getInstance().afficher()[4]);
+	}
+	
+	@Test
+	void testNourrir() {
+		List<CagePOJO> res = Manager.getInstance().getAnimaux();
+		double poidsGaga = res.get(0).getPoids()+0.8;
+		Manager.getInstance().nourrir();
+		assertEquals(poidsGaga, res.get(0).getPoids());
 	}
 
 }
